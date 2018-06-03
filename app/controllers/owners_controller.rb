2ef519463d7fa122561a86c4231845a574a67af1
Joseph Parker <jparker010190@gmail.com>
2018-06-03 00:04:28 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -17,6 +17,7 @@ class OwnersController < ApplicationController
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     @owner.save
+    binding.pry
     redirect to "owners/#{@owner.id}"
   end
 
