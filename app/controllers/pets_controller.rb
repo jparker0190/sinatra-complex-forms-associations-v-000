0128ebe84fa65f66da6da027f04b102d333e8d3a
Joseph Parker <jparker010190@gmail.com>
2018-06-03 00:27:00 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,12 +4,12 @@ class PetsController < ApplicationController
      @pets = Pet.all
      erb :'/pets/index'
    end
- 
+
    get '/pets/new' do
      @owners = Owner.all
      erb :'/pets/new'
    end
- 
+
    post '/pets' do
      if !params[:owner][:name].empty?
        @owner = Owner.create(params[:owner])
@@ -17,31 +17,31 @@ class PetsController < ApplicationController
      else
        @pet = Pet.create(params[:pet])
      end
- 
+
      redirect to "pets/#{@pet.id}"
    end
- 
+
    get '/pets/:id/edit' do
      @pet = Pet.find(params[:id])
      @owners = Owner.all
      erb :'/pets/edit'
    end
- 
+
    get '/pets/:id' do
      @pet = Pet.find(params[:id])
      erb :'/pets/show'
    end
- 
+
    post '/pets/:id' do
      @pet = Pet.find(params[:id])
- 
+
      if params[:owner][:name] != @pet.owner.name
        @owner = Owner.create(params[:owner])
        @pet.update(name: params[:pet][:name], owner: @owner)
      else
        @pet.update(params[:pet])
      end
- 
+
      redirect to "pets/#{@pet.id}"
    end
  end
